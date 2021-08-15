b286119d1861fb14e77c0ab82cdb9f9b1f6c74cb
qedk <1994constant@gmail.com>
2021-08-15 19:41:13 +0530

    Update packages

--- contracts/OpenSignalShares.sol
+++ contracts/OpenSignalShares.sol
@@ -2,11 +2,10 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract OpenSignalShares is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+contract OpenSignalShares is ERC20, Ownable, ERC20Permit {
     constructor()
         ERC20("OpenSignal Shares", "OSS")
         ERC20Permit("OpenSignal Shares")
@@ -14,5 +13,9 @@ contract OpenSignalShares is ERC20, ERC20Burnable, Ownable, ERC20Permit {
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -17,5 +17,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.7",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 9999
+      }
+    }
+  }
 };
