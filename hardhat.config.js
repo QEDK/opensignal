437dc0befcbcf869c3506f8601d6872840e230f6
qedk <1994constant@gmail.com>
2021-08-17 01:30:42 +0530

    Update configuration files

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,14 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle')
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners()
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address)
   }
-});
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -18,7 +18,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: {
-    version: "0.8.7",
+    version: '0.8.7',
     settings: {
       optimizer: {
         enabled: true,
@@ -26,4 +26,4 @@ module.exports = {
       }
     }
   }
-};
+}
