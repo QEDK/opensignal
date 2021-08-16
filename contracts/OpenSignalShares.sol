8718f0e4ce1a8e273e738a606c510f0030f294c9
qedk <1994constant@gmail.com>
2021-08-17 01:29:52 +0530

    :wrench: Fix logic and linting

--- contracts/OpenSignalShares.sol
+++ contracts/OpenSignalShares.sol
@@ -6,12 +6,20 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 contract OpenSignalShares is ERC20, Ownable, ERC20Permit {
+    bool public isEnabled;
+
     constructor()
         ERC20("OpenSignal Shares", "OSS")
-        ERC20Permit("OpenSignal Shares")
-    {}
+        ERC20Permit("OpenSignal Shares") {
+        isEnabled = true;
+    }
+
+    function disable() public onlyOwner {
+        isEnabled = false;
+    }
 
     function mint(address to, uint256 amount) public onlyOwner {
+        require(isEnabled, "CONTRACT_DISABLED");
         _mint(to, amount);
     }
 
