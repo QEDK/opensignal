a2056e17c6357fe9413f944478e91bda0d80c7dd
karbafoo <karbafooo@gmail.com>
2021-08-21 14:07:54 +0430

    basic folder structure

--- frontend/opensignal-web/src/App.tsx
+++ frontend/opensignal-web/src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import 'semantic-ui-css/semantic.min.css';
+import './App.scss';
+import {GitcoinProvider} from './store';
+import {Screens} from './screens';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <GitcoinProvider>
+            <Screens />
+        </GitcoinProvider>
+    );
 }
 
 export default App;
