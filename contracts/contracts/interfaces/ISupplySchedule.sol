5685038f683e97a5aefc73a154510fff7f77fbb6
Amirjab21 <30866671+Amirjab21@users.noreply.github.com>
2021-09-04 13:38:37 +0100

    added rewards distribution that tracks how much Signal each address has staked. The OpenSignal contract calls the reward distribution contract to update the mapping that stores how much each user has staked

---
+++ contracts/contracts/interfaces/ISupplySchedule.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.7;
+
+// https://docs.synthetix.io/contracts/source/interfaces/isupplyschedule
+interface ISupplySchedule {
+    // Views
+    function mintableSupply() external view returns (uint);
+
+    function isMintable() external view returns (bool);
+
+    function minterReward() external view returns (uint);
+
+    // Mutative functions
+    function recordMintEvent(uint supplyMinted) external returns (bool);
+}
