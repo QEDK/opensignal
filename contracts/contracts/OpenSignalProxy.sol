bda27bd35591eab0f200fae0e885fc480a758d8e
Amirjab21 <30866671+Amirjab21@users.noreply.github.com>
2021-09-15 01:01:28 +0100

    Open signal Proxy created

---
+++ contracts/contracts/OpenSignalProxy.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract OpenSignalProxy is ERC1967Proxy, Ownable {
+
+  constructor(address _owner, address _logic) public Ownable(_owner) ERC1967Proxy(_logic) {
+
+  }
+
+  function getImplementation() external {
+    _implementation();
+  }
+
+  function upgradeTo(address impl) external onlyOwner {
+    _upgradeTo(impl);
+  }
+
+}
