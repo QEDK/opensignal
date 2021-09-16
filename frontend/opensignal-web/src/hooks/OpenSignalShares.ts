6ebe21cc95fd5c468a32545bfbd7be8a1e778aa6
Cali93 <armut.cumaali@gmail.com>
2021-09-17 00:15:32 +0200

    eslint config + provide feedback on approve & create project

--- frontend/opensignal-web/src/hooks/OpenSignalShares.ts
+++ frontend/opensignal-web/src/hooks/OpenSignalShares.ts
@@ -1,151 +1,144 @@
-import React from 'react';
-import {isAddress} from '../util/eth.util';
-import {CONTRACTS} from './Contract.hook';
-import Web3 from 'web3';
-import {ethers} from 'ethers';
+import React from "react";
+import { isAddress } from "../util/eth.util";
+import { CONTRACTS } from "./Contract.hook";
+import Web3 from "web3";
+import { ethers } from "ethers";
 
 const useGetShareBalance = (
-    deploymentAddr?: string,
-    addr?: string,
-    trigger = false
+  deploymentAddr?: string,
+  addr?: string,
+  trigger = false,
 ) => {
-    const [balance, setbalance] = React.useState<string>('');
-    const [loading, setloading] = React.useState<boolean>(false);
-    const [err, seterr] = React.useState<any>(null);
-    React.useMemo(async () => {
-        if (
-            isAddress(deploymentAddr) &&
-            addr &&
-            isAddress(addr) &&
-            isAddress(addr)
-        ) {
-            setbalance('');
-            setloading(true);
-            seterr(null);
-            try {
-                let myweb3: any = new Web3(window.ethereum);
-                const shareContract = new myweb3.eth.Contract(
-                    CONTRACTS.OpenSignalShares.abi,
-                    deploymentAddr
-                );
+  const [balance, setbalance] = React.useState<string>("");
+  const [loading, setloading] = React.useState<boolean>(false);
+  const [err, seterr] = React.useState<any>(null);
+  React.useMemo(async () => {
+    if (
+      isAddress(deploymentAddr) &&
+      addr &&
+      isAddress(addr) &&
+      isAddress(addr)
+    ) {
+      setbalance("");
+      setloading(true);
+      seterr(null);
+      try {
+        const myweb3: any = new Web3(window.ethereum);
+        const shareContract = new myweb3.eth.Contract(
+          CONTRACTS.OpenSignalShares.abi,
+          deploymentAddr,
+        );
 
-                const balance = await shareContract.methods
-                    .balanceOf(addr)
-                    .call();
+        const balance = await shareContract.methods.balanceOf(addr).call();
 
-                setbalance(ethers.utils.formatEther(balance).toString());
-                setloading(false);
-                seterr(null);
-            } catch (err) {
-                console.log('err', err);
-                setbalance('');
-                setloading(false);
-                seterr(err);
-            }
-        } else {
-            setbalance('');
-            setloading(false);
-            seterr(null);
-        }
-    }, [addr, deploymentAddr, trigger]);
-    return [balance, loading, err];
+        setbalance(ethers.utils.formatEther(balance).toString());
+        setloading(false);
+        seterr(null);
+      } catch (err) {
+        console.log("err", err);
+        setbalance("");
+        setloading(false);
+        seterr(err);
+      }
+    } else {
+      setbalance("");
+      setloading(false);
+      seterr(null);
+    }
+  }, [addr, deploymentAddr, trigger]);
+  return [balance, loading, err];
 };
 
 const useGetShareAllowance = (
-    opensignalContractAddress?: string,
-    shareContractAddress?: string,
-    addr?: string,
-    trigger = false
+  opensignalContractAddress?: string,
+  shareContractAddress?: string,
+  addr?: string,
+  trigger = false,
 ) => {
-    const [allowance, setallowance] = React.useState<string>('');
-    const [loading, setloading] = React.useState<boolean>(false);
-    const [err, seterr] = React.useState<any>(null);
-    React.useMemo(async () => {
-        if (
-            shareContractAddress &&
-            isAddress(shareContractAddress) &&
-            opensignalContractAddress &&
-            isAddress(opensignalContractAddress) &&
-            addr &&
-            isAddress(addr) &&
-            isAddress(addr)
-        ) {
-            setallowance('');
-            setloading(true);
-            seterr(null);
-            try {
-                const shareContract = getShareContract(shareContractAddress);
+  const [allowance, setallowance] = React.useState<string>("");
+  const [loading, setloading] = React.useState<boolean>(false);
+  const [err, seterr] = React.useState<any>(null);
+  React.useMemo(async () => {
+    if (
+      shareContractAddress &&
+      isAddress(shareContractAddress) &&
+      opensignalContractAddress &&
+      isAddress(opensignalContractAddress) &&
+      addr &&
+      isAddress(addr) &&
+      isAddress(addr)
+    ) {
+      setallowance("");
+      setloading(true);
+      seterr(null);
+      try {
+        const shareContract = getShareContract(shareContractAddress);
 
-                const allowance = await shareContract.methods
-                    .allowance(addr, opensignalContractAddress)
-                    .call();
-                console.log('allowance', allowance);
-                setallowance(allowance);
-                setloading(false);
-                seterr(null);
-            } catch (err) {
-                console.log('err123123', err);
-                setallowance('');
-                setloading(false);
-                seterr(err);
-            }
-        } else {
-            setallowance('');
-            setloading(false);
-            seterr(null);
-        }
-    }, [addr, opensignalContractAddress, shareContractAddress, trigger]);
-    return [allowance, loading, err];
+        const allowance = await shareContract.methods
+          .allowance(addr, opensignalContractAddress)
+          .call();
+        console.log("allowance", allowance);
+        setallowance(allowance);
+        setloading(false);
+        seterr(null);
+      } catch (err) {
+        console.log("err123123", err);
+        setallowance("");
+        setloading(false);
+        seterr(err);
+      }
+    } else {
+      setallowance("");
+      setloading(false);
+      seterr(null);
+    }
+  }, [addr, opensignalContractAddress, shareContractAddress, trigger]);
+  return [allowance, loading, err];
 };
 
 const getShareContract = (deploymentAddr: string) => {
-    let myweb3: any = new Web3(window.ethereum);
-    return new myweb3.eth.Contract(
-        CONTRACTS.OpenSignalShares.abi,
-        deploymentAddr
-    );
+  const myweb3: any = new Web3(window.ethereum);
+  return new myweb3.eth.Contract(
+    CONTRACTS.OpenSignalShares.abi,
+    deploymentAddr,
+  );
 };
 
-const useGetTotalSupply = (
-    shareContractAddress?: string,
-    trigger: boolean = false
-) => {
-    const [totalSupply, settotalSupply] = React.useState<string>('');
-    const [loading, setloading] = React.useState<boolean>(false);
-    const [err, seterr] = React.useState<any>(null);
-    React.useMemo(async () => {
-        if (shareContractAddress && isAddress(shareContractAddress)) {
-            settotalSupply('');
-            setloading(true);
-            seterr(null);
-            try {
-                const shareContract = getShareContract(shareContractAddress);
+const useGetTotalSupply = (shareContractAddress?: string, trigger = false) => {
+  const [totalSupply, settotalSupply] = React.useState<string>("");
+  const [loading, setloading] = React.useState<boolean>(false);
+  const [err, seterr] = React.useState<any>(null);
+  React.useMemo(async () => {
+    if (shareContractAddress && isAddress(shareContractAddress)) {
+      settotalSupply("");
+      setloading(true);
+      seterr(null);
+      try {
+        const shareContract = getShareContract(shareContractAddress);
 
-                const totalSupply = await shareContract.methods
-                    .totalSupply()
-                    .call();
-                console.log('totalSupply', totalSupply);
-                settotalSupply(totalSupply);
-                setloading(false);
-                seterr(null);
-            } catch (err) {
-                console.log('err123123', err);
-                settotalSupply('');
-                setloading(false);
-                seterr(err);
-            }
-        } else {
-            settotalSupply('');
-            setloading(false);
-            seterr(null);
-        }
-    }, [shareContractAddress, trigger]);
-    return [totalSupply, loading, err];
+        const totalSupply = await shareContract.methods.totalSupply().call();
+        console.log("totalSupply", totalSupply);
+        settotalSupply(totalSupply);
+        setloading(false);
+        seterr(null);
+      } catch (err) {
+        console.log("err123123", err);
+        settotalSupply("");
+        setloading(false);
+        seterr(err);
+      }
+    } else {
+      settotalSupply("");
+      setloading(false);
+      seterr(null);
+    }
+  }, [shareContractAddress, trigger]);
+  return [totalSupply, loading, err];
 };
 
 export {
-    useGetShareBalance,
-    useGetShareAllowance,
-    getShareContract,
-    useGetTotalSupply,
+  useGetShareBalance,
+  useGetShareAllowance,
+  getShareContract,
+  useGetTotalSupply,
 };
--- frontend/opensignal-web/src/network/ipfs.ts
+++ frontend/opensignal-web/src/network/ipfs.ts
@@ -1,19 +1,19 @@
-import axios from 'axios';
-import {NFTStorage} from 'nft.storage';
-const saveOnIPFSwithNftStorage = async (properties: any, avatar?: any) => {
-    const apiKey =
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGFmRDg4MmY5YzlCZGE2QjMyOTVlZjIwZDFiM0VDNjA4NDJCREQxMTIiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYzMDQ4NTQ2MDM0OSwibmFtZSI6Ik9wZW5TaWduYWwifQ.-Am4LeJJXbE6ONW6NfHdU2qIHGedHNuuIrfZPcpV0jU';
-    const client = new NFTStorage({token: apiKey});
+import axios from "axios";
+import { NFTStorage } from "nft.storage";
+export const saveOnIPFSwithNftStorage = async (
+  properties: any,
+  avatar?: any,
+) => {
+  const apiKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGFmRDg4MmY5YzlCZGE2QjMyOTVlZjIwZDFiM0VDNjA4NDJCREQxMTIiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYzMDQ4NTQ2MDM0OSwibmFtZSI6Ik9wZW5TaWduYWwifQ.-Am4LeJJXbE6ONW6NfHdU2qIHGedHNuuIrfZPcpV0jU";
+  const client = new NFTStorage({ token: apiKey });
 
-    const metadata = await client.store({
-        name: 'OpenSignal',
-        description: 'OpenSignal Project Info',
-        image: avatar || new Blob(),
-        properties: properties,
-    });
+  const metadata = await client.store({
+    name: "OpenSignal",
+    description: "OpenSignal Project Info",
+    image: avatar || new Blob(),
+    properties: properties,
+  });
 
-    return metadata;
+  return metadata;
 };
-
-const saveOnIPFS = saveOnIPFSwithNftStorage;
-export {saveOnIPFS};
