c835a833897187a4596a0062ef4e2cacb6172890
Amirjab21 <30866671+Amirjab21@users.noreply.github.com>
2021-09-19 14:40:11 +0100

    solhint and unused variable

--- contracts/contracts/OpenSignal.sol
+++ contracts/contracts/OpenSignal.sol
@@ -55,7 +55,7 @@ contract OpenSignal is ERC2771Context, ReentrancyGuard {
         minStake = 2 ether;
         reserveRatio = 500000;
         minLockinTimeInEpochs = 3;
-        RewardsDistribution rewardsDistribution = RewardsDistribution(governor, nativeToken, epoch);
+        rewardsDistribution = RewardsDistribution(_rewardsDistribution);
     }
 
     function changeMinLockinTimeInEpochs(uint8 newLockinTime) external onlyGovernor {
