59c2f4c313e24935458be1fbdf64e83fa94d2f08
Amirjab21 <30866671+Amirjab21@users.noreply.github.com>
2021-09-21 15:31:04 +0100

    deploy script and initialization

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -2,16 +2,23 @@ const hre = require('hardhat')
 
 async function main () {
 
-  const RewardsDistribution = await hre.ethers.getContractFactory('RewardsDistribution')
-  const rewardsDistributionContract = await RewardsDistribution.deploy()
-  await rewardsDistributionContract.deployed()
-  console.log('RewardsDistribution deployed to:', rewardsDistributionContract.address);
-
+  const EPOCH_LENGTH = 2592000;
+  const ONWER_ADDRESS = '0x16e9b2B8A2C92e98faBd8f9B08210f674F570059';
 
   const OpenSignalToken = await hre.ethers.getContractFactory('OpenSignalToken')
   const openSignalTokenContract = await OpenSignalToken.deploy()
   await openSignalTokenContract.deployed()
   console.log('OpenSignalToken deployed to:', openSignalTokenContract.address);
+
+  await openSignalTokenContract.initialize();
+
+  const RewardsDistribution = await hre.ethers.getContractFactory('RewardsDistribution')
+  const rewardsDistributionContract = await RewardsDistribution.deploy()
+  await rewardsDistributionContract.deployed()
+  console.log('RewardsDistribution deployed to:', rewardsDistributionContract.address);
+
+  await rewardsDistributionContract.initialize(openSignalTokenContract.address, EPOCH_LENGTH, ONWER_ADDRESS);
+
 
   const OpenSignalShares = await hre.ethers.getContractFactory('OpenSignalShares')
   const openSignalSharesContract = await OpenSignalShares.deploy()
@@ -22,14 +29,14 @@ async function main () {
   const OpenSignal = await hre.ethers.getContractFactory('OpenSignal')
   const openSignalContract = await OpenSignal.deploy(
     // BiconomyForwarder on Rinkeby
-    '0xFD4973FeB2031D4409fB57afEE5dF2051b171104', '0x4bed939d328c4a9b5decff9668b534f44497e601', RewardsDistributionContract.address
+    '0xFD4973FeB2031D4409fB57afEE5dF2051b171104', openSignalTokenContract.address, rewardsDistributionContract.address
   )
   await openSignalContract.deployed()
   console.log('OpenSignal deployed to:', openSignalContract.address);
 
   const OpenSignalProxy = await hre.ethers.getContractFactory('OpenSignalProxy')
   const openSignalProxyContract = await OpenSignalProxy.deploy(
-    '0x16e9b2B8A2C92e98faBd8f9B08210f674F570059', OpenSignal.address
+    ONWER_ADDRESS, openSignalContract.address
   )
   await openSignalProxyContract.deployed()
   console.log('OpenSignalProxy deployed to:', openSignalProxyContract.address);
